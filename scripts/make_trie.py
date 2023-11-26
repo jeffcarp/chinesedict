e0c96dc42194f966364c0231f206d2b788909372
Jeff Carpenter <code@jeffcarp.com>
2023-11-25 22:38:17 -0800

    Experiment with pinyin in trie

--- scripts/make_trie.py
+++ scripts/make_trie.py
@@ -1,11 +1,13 @@
 import csv
 import functools
 import sys
+import unicodedata
 
 # Assuming this is run from project root.
 CEDICT_PARSED_PATH = './data/cedict_parsed.csv'
-DEBUG = False
+DEBUG = True
 TRIE = {}
+STOP_CHAR = None
 
 
 def add_word(word):
@@ -17,15 +19,21 @@ def add_word(word):
 
     trie_pointer = trie_pointer[char]
 
+  # Add stop char.
+  trie_pointer[char] = STOP_CHAR
+
 
 def main():
   with open(CEDICT_PARSED_PATH, 'r+') as f:
     csvreader = csv.reader(f, delimiter='∙', dialect='unix', quoting=csv.QUOTE_MINIMAL)
     if DEBUG:
       csvreader = list(csvreader)[20:40]
-    for simplified, _, _, _ in csvreader:
-      print(simplified)
+    for simplified, pinyin, _, _ in csvreader:
+      print(simplified, pinyin)
       add_word(simplified)
+      pinyin = unicodedata.normalize('NFKD', pinyin).encode('ASCII', 'ignore').decode()
+      print(pinyin)
+      add_word(pinyin)
 
   print('FINAL TRIE:')
   print(TRIE)
